d2931d8eb054cc1efa9cbf25d13794945da6430d
nitesh-dev <niteshdev547@gmail.com>
2023-07-22 19:52:09 +0530

    some features added

--- src/pages/ColorProject.tsx
+++ src/pages/ColorProject.tsx
@@ -2,8 +2,59 @@ import { useEffect, useState } from "react"
 import Color from "../components/Color"
 import '../assets/styles/color-project.css'
 
+function hexToHSL(H: string) {
+    // Convert hex to RGB first
+    let r = 0, g = 0, b = 0;
+    if (H.length === 4) {
+      r = parseInt("0x" + H[1] + H[1]);
+      g = parseInt("0x" + H[2] + H[2]);
+      b = parseInt("0x" + H[3] + H[3]);
+    } else if (H.length === 7) {
+      r = parseInt("0x" + H[1] + H[2]);
+      g = parseInt("0x" + H[3] + H[4]);
+      b = parseInt("0x" + H[5] + H[6]);
+    }
+  
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const cmin = Math.min(r, g, b);
+    const cmax = Math.max(r, g, b);
+    const delta = cmax - cmin;
+    let h = 0;
+    let s = 0;
+    let l = 0;
+  
+    if (delta === 0)
+      h = 0;
+    else if (cmax === r)
+      h = ((g - b) / delta) % 6;
+    else if (cmax === g)
+      h = (b - r) / delta + 2;
+    else
+      h = (r - g) / delta + 4;
+  
+    h = Math.round(h * 60);
+  
+    if (h < 0)
+      h += 360;
+  
+    l = (cmax + cmin) / 2;
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+  
+    return {
+        hue: h, saturation: s, lightness: l
+    }
+  }
+  
+  
+
 function ColorProject() {
 
+    const [selectedColor, setSelectedColor] = useState('#000000')
     const [colors, setColors] = useState(Array<string>())
 
     useEffect(() => {
@@ -12,14 +63,19 @@ function ColorProject() {
         setColors(newColors)
     }, [])
 
+    function onSelectedColorChange(event: any){
+        setSelectedColor(event.target.value)
+    }
+
 
     return (
         <>
             <div className="page-holder">
                 <div className="container">
                     <div className="input-color-holder">
-                        <span>#000000</span>
-                        <input type="color" />
+                        <span>{selectedColor}</span>
+                        <input value={selectedColor} onChange={onSelectedColorChange} type="color" />
+                        <div style={{backgroundColor:selectedColor}}></div>
                     </div>
 
                     <div className="colors-container">
